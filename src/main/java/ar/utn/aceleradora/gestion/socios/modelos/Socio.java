c21a910e80aa6418747c63e4bd2192dc3fc8742e
OviedoFrancoAlexander <64283850+OviedoFranc@users.noreply.github.com>
2023-09-26 15:42:43 -0300

    Fix bug socio

--- src/main/java/ar/utn/aceleradora/gestion/socios/modelos/Socio.java
+++
@@ -1,4 +0,0 @@
-package ar.utn.aceleradora.gestion.socios.modelos;
-
-public interface Socio {
-}
--- src/main/java/ar/utn/aceleradora/gestion/socios/modelos/TipoSocio.java
+++
@@ -1,6 +0,0 @@
-package ar.utn.aceleradora.gestion.socios.modelos;
-
-public enum TipoSocio {
-  SOCIO_EMPRESA,
-  SOCIO_PLENARIO
-}
